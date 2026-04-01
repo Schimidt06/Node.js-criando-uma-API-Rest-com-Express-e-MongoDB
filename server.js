64835954714c629004158ed79dafa3b50e3a2980
Joao <jpschimidtfuentes@gmail.com>
2026-04-01 15:05:44 -0300

    Atualização X

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ import http from 'http';
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Olá, mundo!');
+    res.end('Cursando Node.js');
+});
+
+server.listen(3000, () => { 
+    console.log('Servidor rodando na porta 3000');
 });
