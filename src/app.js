18c3707a31fb79f2bd55b21f6381c96e297b710d
Joao <jpschimidtfuentes@gmail.com>
2026-04-08 16:53:30 -0300

    Atualização y

--- src/app.js
+++ src/app.js
@@ -1,9 +1,43 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
+const livros = [
+    {
+        id: 1,
+        titulo: "Livro 1",
+
+    },
+    {
+        id: 2,
+        titulo: "Livro 2",
+        
+    }
+]
+
+function buscarLivro(id) {
+    return livros.find(l => l.id === id);
+}
 app.get('/', (req, res) => {
     res.status(200).send('Cursando Express API');
 });
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.get('/livros/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const livro = livros.find(l => l.id === id);
+    res.status(200).json(livro);
+});
+
+app.post('/livros', (req, res) => {
+   livros.push(req.body);
+    res.status(201).send('Livro adicionado com sucesso');
+});
+
+
 
 export default app;
